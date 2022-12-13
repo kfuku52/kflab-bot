0d3ae39f5b826349abbdd276db87a69289ffb87b
Kenji Fukushima <kfuku52@gmail.com>
2022-12-13 18:44:49 +0100

    update

--- write_inactive_issue.py
+++ write_inactive_issue.py
@@ -6,6 +6,7 @@ print('Starting write_inactive_issue.py')
 
 hub_out_file = sys.argv[1]
 since_last_updated_sec = int(sys.argv[2])
+remove_label = sys.argv[3]
 
 with open(hub_out_file, 'r') as f:
     hub_txt = f.read()
@@ -14,18 +15,20 @@ hub_items = hub_txt.split('\n')
 if hub_items[len(hub_items)-1]=='':
     hub_items = hub_items[0:(len(hub_items)-1)]
 
-num_open_issue = int(len(hub_items)/6)
+num_item = 7
+num_open_issue = int(len(hub_items)/num_item)
 print('Number of open Issues: {:,}'.format(num_open_issue))
 
 issues = list()
 for i in range(num_open_issue):
     issues.append(dict())
-    issues[i]['issue_number'] = int(hub_items[i*6+0])
-    issues[i]['assignees'] = hub_items[i*6+1].split(',')
-    issues[i]['relative_time_updated'] = hub_items[i*6+2]
-    issues[i]['unix_timestamp_updated'] = int(hub_items[i*6+3])
-    issues[i]['issue_title'] = hub_items[i*6+4]
-    issues[i]['issue_url'] = hub_items[i*6+5]
+    issues[i]['issue_number'] = int(hub_items[i*num_item+0])
+    issues[i]['assignees'] = hub_items[i*num_item+1].split(', ')
+    issues[i]['relative_time_updated'] = hub_items[i*num_item+2]
+    issues[i]['unix_timestamp_updated'] = int(hub_items[i*num_item+3])
+    issues[i]['issue_title'] = hub_items[i*num_item+4]
+    issues[i]['issue_url'] = hub_items[i*num_item+5]
+    issues[i]['labels'] = hub_items[i*num_item+6].split(', ')
 
 inactive_issues = [ issue for issue in issues if (time.time()-issue['unix_timestamp_updated']) > since_last_updated_sec ]
 print('Number of inactive Issues: {:,}'.format(len(inactive_issues)))
@@ -37,7 +40,7 @@ for assignee in unique_assignees:
     assigned_issue_nums = [ issue['issue_number'] for issue in assigned_issues ]
     print('Issues assigned to {}: {}'.format(assignee, ','.join([ str(n) for n in assigned_issue_nums ])))
     assignee_txt = ''
-    assignee_txt += ':wave: Hi @{}, As an assignee, you are expected to post an update in the following inactive issues.\n'.format(assignee)
+    assignee_txt += ':wave: @{}: '.format(assignee)
     for assigned_issue in assigned_issues:
         inactive_day = int((time.time() - assigned_issue['unix_timestamp_updated']) / 86400)
         assignee_txt += '{} (>{} days), '.format(assigned_issue['issue_url'], inactive_day)
